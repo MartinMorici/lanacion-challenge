1988d239a49642d4263849bc8d356dc2704edbcd
Martín Morici <martinmorici3@gmail.com>
2023-04-05 20:53:22 -0300

    layout desktop terminado

---
+++ components/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import MenuIcon from '../images/menu.svg'
+import SearchIcon from '../images/search.svg'
+import Logo from '../images/logo.svg'
+import Image from 'next/image'
+const Navbar = () => {
+  return (
+    <nav className='shadow-md z-50'>
+        <div className='max-w-[1280px] mx-auto flex justify-between py-5 text-sm'>
+            <div className='flex justify-center items-center'>
+                <div className='flex gap-3 items-center'>
+                    <Image src={MenuIcon} alt="Icono menu hamburguesa" width={35} />
+                    <span>MENÚ</span>
+                </div>
+                <div className='relative flex items-center justify-center'>
+                    <input className='ml-10 border h-10  border-gray-300 rounded-md px-3' type="text" placeholder='Buscar' />
+                    <Image className='block absolute right-3' width={22} src={SearchIcon} alt="Icono busqueda"/>
+                </div>
+            </div>
+
+            <Image src={Logo} width={300} alt='Logo La Nación'/>
+
+            <div className='flex gap-6'>
+                <button className='bg-yellow-300 border border-black px-4 text-black rounded-md'>SUSCRIBITE</button>
+                <button className='border border-blue-700 px-4 text-blue-700 rounded-md'>INGRESAR</button>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
+import Navbar from '@/components/Navbar';
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import ArticleImage from '../images/article.jpg'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -14,110 +13,85 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+
+        <Navbar/>
+        <main className='z-[-1] relative' >
+          <div className='bg-gray-200 h-44 w-full'>
+            <div className=' bg-blue-300 h-full max-w-[1280px] mx-auto'></div>
           </div>
-        </div>
+          <section className='grid grid-cols-[1fr_280px] max-w-[1280px] mx-auto mt-12'>
+            <div className='mr-20'>
+              <h1 className='underline text-4xl'>Acumulado Grilla</h1>
+              <div className='mt-4 mb-6 flex items-center gap-2'>
+                <span>Platos principales</span> 
+                <span className="font-bold text-2xl">·</span> 
+                <span>Cerdo</span> 
+                <span className="font-bold text-2xl">·</span> 
+                <span>Papas</span> 
+                <span className="font-bold text-2xl">·</span> 
+                <span>Date un gustito</span> 
+                <span className="font-bold text-2xl">·</span> 
+                <span>La Familia</span>
+              </div>
+              <div className='grid grid-cols-3 gap-10 justify-between items-center'>
+                <article className=''>
+                  <Image src={ArticleImage} alt="Imagen Articulo"/>
+                  <p><span className="font-bold">La escuela.</span> que tiene de escudo al Che Guevara y donde izan la bandera de Cuba</p>
+                  <span className='text-[15px] text-gray-500'>1 de Julio de 2019</span>
+                </article>
+                <article className=''>
+                  <Image src={ArticleImage} alt="Imagen Articulo"/>
+                  <p><span className="font-bold">La escuela.</span> que tiene de escudo al Che Guevara y donde izan la bandera de Cuba</p>
+                  <span className='text-[15px] text-gray-500'>1 de Julio de 2019</span>
+                </article>
+                <article className=''>
+                  <Image src={ArticleImage} alt="Imagen Articulo"/>
+                  <p><span className="font-bold">La escuela.</span> que tiene de escudo al Che Guevara y donde izan la bandera de Cuba</p>
+                  <span className='text-[15px] text-gray-500'>1 de Julio de 2019</span>
+                </article>
+                <article className=''>
+                  <Image src={ArticleImage} alt="Imagen Articulo"/>
+                  <p><span className="font-bold">La escuela.</span> que tiene de escudo al Che Guevara y donde izan la bandera de Cuba</p>
+                  <span className='text-[15px] text-gray-500'>1 de Julio de 2019</span>
+                </article>
+                <article className=''>
+                  <Image src={ArticleImage} alt="Imagen Articulo"/>
+                  <p><span className="font-bold">La escuela.</span> que tiene de escudo al Che Guevara y donde izan la bandera de Cuba</p>
+                  <span className='text-[15px] text-gray-500'>1 de Julio de 2019</span>
+                </article>
+                <article className=''>
+                  <Image src={ArticleImage} alt="Imagen Articulo"/>
+                  <p><span className="font-bold">La escuela.</span> que tiene de escudo al Che Guevara y donde izan la bandera de Cuba</p>
+                  <span className='text-[15px] text-gray-500'>1 de Julio de 2019</span>
+                </article>
+                <article className=''>
+                  <Image src={ArticleImage} alt="Imagen Articulo"/>
+                  <p><span className="font-bold">La escuela.</span> que tiene de escudo al Che Guevara y donde izan la bandera de Cuba</p>
+                  <span className='text-[15px] text-gray-500'>1 de Julio de 2019</span>
+                </article>
+                <article className=''>
+                  <Image src={ArticleImage} alt="Imagen Articulo"/>
+                  <p><span className="font-bold">La escuela.</span> que tiene de escudo al Che Guevara y donde izan la bandera de Cuba</p>
+                  <span className='text-[15px] text-gray-500'>1 de Julio de 2019</span>
+                </article>
+              </div>
+            </div>
+            <div className='h-full bg-blue-300 '>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
+            </div>
+          </section>
+        </main>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
     </>
   )
 }
+
+export async function getStaticProps() {
+  
+
+  return {
+    props: {                      
+    }               
+  }; 
+}
+  
